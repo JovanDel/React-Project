343638786a4761e0d7ae1fa74d577a039124cbf1
Jovan Delport <jovan@imgn.africa>
2022-07-07 09:44:14 +0200

    Additional commit. Creating routes and links. Context API, useContext Hook & Deployment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
+ import { v4 as uuidv4 } from 'uuid';
+ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
  import { useState } from 'react';
  import Header from'./components/Header';
  import FeedbackList from './components/FeedbackList';
  import FeedbackStats from './components/FeedbackStats';
- import FeedbackData from './data/FeedbackData';
  import FeedbackForm from './components/FeedbackForm';
- 
+ import AboutIconLink from './components/AboutIconLink';
+ import AboutPage from './pages/AboutPage';
+ import {FeedbackProvider} from './context/FeedbackContext';
+ import FeedbackData from './data/FeedbackData';
+
  function App() {
      const [feedback, setFeedback] = useState
      (FeedbackData)
+
+     const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4()
+        setFeedback([newFeedback, ...feedback])
+     }
 
      const deleteFeedback = (id) => {
          if(window.confirm('Are you sure you want to delete this item')) {
@@ -16,16 +26,29 @@
      }
      
      return (
-         <>
-        <Header />
+        <FeedbackProvider>
+        <Router>
+         <Header />
          <div className='container'>
-        <FeedbackForm />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} 
-         handleDelete={deleteFeedback} />
-        </div>
-    </>
-     )
+            <Routes>
+            <Route exact path='/' element={
+                <>
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats />
+        <FeedbackList handleDelete={deleteFeedback} />
+                </>
+            }>
+        
+         </Route>
+
+         <Route path='/about' element={<AboutPage />} />
+         </Routes>
+
+         <AboutIconLink />
+ </div>
+    </Router>
+    </FeedbackProvider>
+    )
  }
 
  export default App
--- src/components/shared/Button.jsx
+++ src/components/shared/Button.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 function Button({ children, version, type, isDisabled }) {
     return (
         <button type={type} disabled={isDisabled} 
-        className={'btn btn-${version}'}>
+        className={`btn btn-${version}`}>
            {children} 
         </button>
     )
